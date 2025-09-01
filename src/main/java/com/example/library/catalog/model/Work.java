0d007f519aa8f38d1c09ddb0f356e1a7e70ee43c
ayan0138 <ayah0002@stud.ek.dk>
2025-09-01 13:56:58 +0200

    trying commit messages

--- src/main/java/com/example/library/catalog/model/Work.java
+++ src/main/java/com/example/library/catalog/model/Work.java
@@ -16,6 +16,51 @@ public class Work {
     private String authors;
     private String subjects;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public WorkType getWorkType() {
+        return workType;
+    }
+
+    public void setWorkType(WorkType workType) {
+        this.workType = workType;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(String subjects) {
+        this.subjects = subjects;
+    }
 }
