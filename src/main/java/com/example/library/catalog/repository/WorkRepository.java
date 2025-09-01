ae768bfc2510114f5a2491636dab9cf1b08ee9c4
ayan0138 <ayah0002@stud.ek.dk>
2025-09-01 14:59:36 +0200

    workservice updating

--- src/main/java/com/example/library/catalog/repository/WorkRepository.java
+++ src/main/java/com/example/library/catalog/repository/WorkRepository.java
@@ -1,4 +1,10 @@
 package com.example.library.catalog.repository;
 
-public class WorkRepository {
+import com.example.library.catalog.model.Work;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WorkRepository extends JpaRepository<Work, Long> {
+    List<Work> findByTitleContaining(String title);
 }
--- src/main/java/com/example/library/catalog/service/WorkService.java
+++ src/main/java/com/example/library/catalog/service/WorkService.java
@@ -1,4 +1,54 @@
 package com.example.library.catalog.service;
 
+import com.example.library.catalog.model.Work;
+import com.example.library.catalog.repository.WorkRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+
+@Service
 public class WorkService {
+
+    private final WorkRepository workRepository;
+
+    public WorkService(WorkRepository workRepository) {
+        this.workRepository = workRepository;
+}
+    public void createWork(Work work) {
+        return;
+
+}
+    public List<Work> getAllWorks(String title) {
+        return workRepository.findByTitleContaining(title);
+}
+
+    public Work getWorkById(Long id) {
+        Optional<Work> work = workRepository.findById(id);
+        if (work.isPresent()) {
+            return work.get();
+
+}
+    throw new RuntimeException("Work not found with id:" + id ) ;
+
+
+}
+    public void updateWork(Long id, Work work)  {
+        return;
+    
+}
+    public void deleteWork(Long id) {
+        if (workRepository.existsById(id)) {
+            workRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Work not found with this id:" + id) ;
+        }
+
+
+}
+     public void searchWorks(String title) {
+        return;
+}
 }
